a7611688cdb4a55926af1b03778063a172cd8b26
Toby Potter <tpotter@athena.pawsey.org.au>
2018-04-17 16:52:25 +0800

    small change to profiling

--- profiling/build_for_map.sh
+++ profiling/build_for_map.sh
@@ -2,4 +2,4 @@
 
 module load intel intel-opencl-sdk broadwell mvapich forge
 
-cmake -DCMAKE_INSTALL_PREFIX=../install .. -DOpenCL_LIBRARY=/pawsey/opencl-sdk/7.0.0/opencl/SDK/lib64/libOpenCL.so -DCMAKE_EXE_LINKER_FLAGS="-L/home/tpotter -lmap-sampler-pmpi -lmap-sampler -Wl,--eh-frame-hdr -Wl,-rpath=/home/tpotter" -DCMAKE_BUILD_TYPE=debug
+cmake -DCMAKE_INSTALL_PREFIX=../install .. -DOpenCL_LIBRARY=/pawsey/opencl-sdk/7.0.0/opencl/SDK/lib64/libOpenCL.so -DCMAKE_EXE_LINKER_FLAGS="-L${HOME} -lmap-sampler-pmpi -lmap-sampler -Wl,--eh-frame-hdr -Wl,-rpath=${HOME}" -DCMAKE_BUILD_TYPE=debug
